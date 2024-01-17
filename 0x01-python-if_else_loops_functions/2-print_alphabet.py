848cb535244f77b827097fa0010af83a527e64fa
AquillaAf <aquillaafintinni@gmail.com>
2024-01-17 09:21:23 +0000

    ascii_Alpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ascii_code in range(ord('a'), ord('z')+1):
-        print(chr(ascii_code), end="")
+for i in range(ord('a'), ord('z')+1):
+    print("{}".format(chr(i)), end="")
 
