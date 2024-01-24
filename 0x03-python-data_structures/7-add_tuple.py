159182b8fb7197dc50b7f5d1ccd94669f3dd84d2
AquillaAf <aquillaafintinni@gmail.com>
2024-01-24 09:28:16 +0000

    adding tuples mem

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) > 1 and len(tuple_b) == 1:
+        sum_1 = tuple_a[0] + tuple_b[0]
+        sum_2 = tuple_a[1] + 0
+    elif len(tuple_a) == 1 and len(tuple_b > 1):
+        sum_1 = tuple_a[0] + tuple_b[0]
+        sum_2 = 0 + tuple_b[1]
+    elif len(tuple_a) == 0:
+        return(tuple_b)
+    elif len(tuple_b) == 0:
+        return (tuple_a)
+    elif len(tuple_a) > 1 and len(tuple_b) > 1:
+        sum_1 = tuple_a[0] + tuple_b[0]
+        sum_2 = tuple_a[1] + tuple_b[1]
+    return (sum_1, sum_2)
