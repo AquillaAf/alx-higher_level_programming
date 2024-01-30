6edee7222d57bc723e9fa7e5e0bd6349b9eb4750
AquillaAf <aquillaafintinni@gmail.com>
2024-01-30 11:38:55 +0000

    Revert "removed static lib dir"
    This reverts commit 1507eb4fc7b65f0d1eb6771938eda5c1ee77481e.

--- 0x04-python-more_data_structures/1-search_replace.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    copy = my_list.copy()
-    copy[copy.index(search)] = replace
-    return copy
--- 0x04-python-more_data_structures/2-uniq_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-
-    var = 0
-    unique = my_list.unique()
-    for i in unique:
-        var += 0
-    return var
