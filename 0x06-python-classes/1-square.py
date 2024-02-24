b556a70bb52357d06b8cbc242013a9cb817f0ae0
AquillaAf <aquillaafintinni@gmail.com>
2024-02-24 16:41:00 +0000

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creating a class."""
+class Square:
+    """represeents a square"""
+    def __init__(self, size):
+        """initializes a new square
+        args:
+            size(int): the size of the square
+        """
+        self.__size = size
