6c8ffe22fc92bd3eeb46496193f533c897d03cf6
AquillaAf <aquillaafintinni@gmail.com>
2024-06-21 12:58:05 +0000

    value of argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
-#!/usr/bin/node
-const args = process.argv;
-if (args.length === 2) {
+#!/usr/bin/env node
+const args = process.argv.slice(2);  // Remove the first two elements (node executable and script path)
+const firstArg = args.shift();  // Remove and retrieve the first argument
+
+if (firstArg === undefined) {
   console.log('No argument');
-} else if (args.length > 2) {
-  for (let i = 2; i < args.length; i++) {
-    console.log(args[i]);
-  }
+} else {
+  console.log(firstArg);
 }
