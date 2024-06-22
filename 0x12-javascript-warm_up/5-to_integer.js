23e2bddc1cefabebf2b3885264b8a3388582c192
AquillaAf <aquillaafintinni@gmail.com>
2024-06-22 17:54:43 +0000

    An integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args.length === 2) {
+  console.log('Not a number');
+} else if (args.length === 3) {
+  const value = Number(args[2]);
+  if (isNaN(value) === true) {
+    console.log('Not a number');
+  } else {
+    console.log(value);
+  }
+}
