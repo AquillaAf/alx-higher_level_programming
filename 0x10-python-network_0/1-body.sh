108fa1ac6e8d80431a9b6fafbb616cf0915c4a98
AquillaAf <aquillaafintinni@gmail.com>
2024-07-10 13:03:59 +0000

    response body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-	echo "no curl provided"
-	exit 1
-fi
-prompt=$1
-var=$(curl -s -w "%{http_code}" -o response_body.txt $prompt)
-http_code=${var}
-if [ "$http_code" -eq 200 ]; then 
-	cat response_body.txt
-else
-	echo " failed to retreive data, status code: $http_code"
-fi	
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
