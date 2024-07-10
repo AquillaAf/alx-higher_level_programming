11311d802d894214113490f98b2a80fcd8b47069
AquillaAf <aquillaafintinni@gmail.com>
2024-07-10 13:08:31 +0000

    body count

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-	echo "No url found"
-	exit 1
-fi
-var=$(curl -s  "$1" | wc -c)
-echo "$var"
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
